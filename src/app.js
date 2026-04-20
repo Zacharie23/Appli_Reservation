8a47f9b9b15c806e033aacf448cb796c5a52bd70
Romainns <romainsintas@gmail.com>
2026-04-20 16:12:46 +0200

    Modification du app.js pour intégration front dans image

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const cors = require('cors');
 
@@ -20,6 +21,7 @@ const userRoutes = require('./routes/UserRoutes');
 setupSwagger(app);
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.use('/events', routes);
 app.use('/auth', authRoutes);
 app.use('/reservations', reservationRoutes);
